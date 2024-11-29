77ba12d93118b9c9ecf33fcd5a0d8ab98a6ed585
SaraSorlin <sorlin82@gmail.com>
2024-11-29 14:40:51 +0100

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,15 @@
 // backend
 import express from 'express';
 import path from 'path';
+import { fileURLToPath } from 'url';
 const app = express();
 const port = 4000;
+
+// Nedan tillagt med hjälp av max för att fungera för oss två
+//#######################################################
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+//##########################################################
 
 // log all request to the server
 app.use((req, res, next) => {
@@ -15,16 +22,37 @@ app.get('/api/*', async (req, res) => {
   res.json(await (await fetch(`https://www.willys.se${req.url.slice(4)}`)).json())
 });
 
-// Category tree: /api/leftMenu/categorytree 
+// Category tree: /api/leftMenu/categorytree
 // Category list: /api/c/kott-chark-och-fagel/palagg?size=30&page=0&sort=
 // [topRated, name-asc,name-desc,price-asc,price-desc,compareprice-asc,compareprice-desc]
 // Product info:  /api/axfood/rest/p/101290312_ST
 
 // serve built application (dist folder)
-const distFolder = path.join(import.meta.dirname, '..', 'dist');
+
+
+
+
+// Nedan ändrat med hjälp av max för att fungera för oss två funkar inte det får man avkommentera det och kommentera ut det andra
+//#######################################################
+//const distFolder = path.join(import.meta.dirname, '..', 'dist');
+const distFolder = "../dist";
+//###########################################################
+
+
+
+
 app.use(express.static(distFolder));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(distFolder, 'index.html'));
+
+
+  // Nedan ändrat med hjälp av max för att fungera för oss två funkar inte det får man avkommentera det och kommentera ut det andra
+  //#######################################################
+  //res.sendFile(path.join(distFolder, 'index.html'));
+  res.sendFile(path.resolve(__dirname, "../dist", 'index.html'));
+  //#############################################################
+
+
+
 });
 
 
